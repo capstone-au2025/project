709b3777678e3587c8b75d3582674eca67d6b68a
Mark Bundschuh <mark@mbund.dev>
2025-09-16 16:11:14 -0400

    Connect to Bedrock (#2)
    Connect to Bedrock and do inference, simply on `/api/hello` for testing.
    Also implement mock inference provider to fall back to if AWS is not
    fully configured.
    
    Assumes role defined by `$AWS_BEDROCK_ROLE_ARN`. Inference is done based
    on model ID `$AWS_BEDROCK_MODEL_ID` such as
    `meta.llama3-3-70b-instruct-v1:0`. The list of model IDs are
    [here](https://docs.aws.amazon.com/bedrock/latest/userguide/models-supported.html).
    
    Example with AWS configured:
    <img width="1069" height="283" alt="image"
    src="https://github.com/user-attachments/assets/4a68d27f-86a3-4cb5-8927-fab33df7ee03"
    />
    
    Example with AWS not configured:
    <img width="1069" height="283" alt="image"
    src="https://github.com/user-attachments/assets/1508063c-85e9-4804-8eaa-5f30af53d7d9"
    />

---
+++ backend/inference.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"text/template"
+	"time"
+
+	_ "embed"
+)
+
+type InferenceProvider interface {
+	// Runs user context input through inference provider.
+	// A system prompt may be included on creation of the inference provider.
+	Infer(ctx context.Context, input string) (string, error)
+}
+
+type MockInferenceProvider struct{}
+
+var _ InferenceProvider = (*MockInferenceProvider)(nil)
+
+func NewMockInferenceProvider() *MockInferenceProvider {
+	return &MockInferenceProvider{}
+}
+
+func (m *MockInferenceProvider) Infer(ctx context.Context, input string) (string, error) {
+	time.Sleep(2 * time.Second)
+	return "MOCKED INFERENCE PROVIDER\n\n" + input + "\n\nMOCKED INFERENCE PROVIDER", nil
+}
+
+//go:embed prompt.txt
+var systemPromptTemplateContent string
+var systemPromptTemplate *template.Template
+
+func init() {
+	systemPromptTemplate = template.Must(template.New("prompt.txt").Parse(systemPromptTemplateContent))
+}
+
+func RenderSystemPrompt() string {
+	loc, _ := time.LoadLocation("America/New_York")
+	now := time.Now().In(loc).Format("Monday, January 2 15:04:05 MST 2006")
+
+	var buf bytes.Buffer
+	err := systemPromptTemplate.Execute(&buf, map[any]any{
+		"CurrentTime": now,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	return buf.String()
+}
