f43010af1565f4b38b078f1a91282c587ee4738a
Mark Bundschuh <mark@mbund.dev>
2025-09-16 15:16:54 -0400

    connect to bedrock

---
+++ backend/aws.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials/stscreds"
+	"github.com/aws/aws-sdk-go-v2/service/bedrockruntime"
+	"github.com/aws/aws-sdk-go-v2/service/bedrockruntime/types"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+)
+
+type AWS struct {
+	brc     *bedrockruntime.Client
+	modelId string
+}
+
+var _ InferenceProvider = (*AWS)(nil)
+
+func NewAWS() (*AWS, error) {
+	region := os.Getenv("AWS_REGION")
+	if region == "" {
+		return nil, fmt.Errorf("environment variable AWS_REGION is not defined")
+	}
+
+	bedrockRoleArn := os.Getenv("AWS_BEDROCK_ROLE_ARN")
+	if bedrockRoleArn == "" {
+		return nil, fmt.Errorf("environment variable AWS_BEDROCK_ROLE_ARN is not defined")
+	}
+
+	bedrockModelId := os.Getenv("AWS_BEDROCK_MODEL_ID")
+	if bedrockModelId == "" {
+		return nil, fmt.Errorf("environment variable AWS_BEDROCK_MODEL_ID is not defined")
+	}
+
+	ctx := context.Background()
+
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(region))
+	if err != nil {
+		return nil, fmt.Errorf("failed to load default AWS config: %v", err)
+	}
+
+	stsClient := sts.NewFromConfig(cfg)
+	assumeRoleProvider := stscreds.NewAssumeRoleProvider(stsClient, bedrockRoleArn)
+	assumedCfg := cfg.Copy()
+	assumedCfg.Credentials = aws.NewCredentialsCache(assumeRoleProvider)
+
+	brc := bedrockruntime.NewFromConfig(assumedCfg)
+
+	return &AWS{
+		brc:     brc,
+		modelId: bedrockModelId,
+	}, nil
+}
+
+func (b *AWS) Infer(ctx context.Context, input string) (string, error) {
+	systemPrompt := RenderSystemPrompt()
+
+	response, err := b.brc.Converse(ctx, &bedrockruntime.ConverseInput{
+		ModelId: aws.String(b.modelId),
+		Messages: []types.Message{
+			{
+				Content: []types.ContentBlock{
+					&types.ContentBlockMemberText{Value: input},
+				},
+				Role: "user",
+			},
+		},
+		System: []types.SystemContentBlock{
+			&types.SystemContentBlockMemberText{Value: systemPrompt},
+		},
+	})
+	if err != nil {
+		return "", fmt.Errorf("failed to converse with Bedrock: %v", err)
+	}
+
+	responseText, _ := response.Output.(*types.ConverseOutputMemberMessage)
+	responseContentBlock := responseText.Value.Content[0]
+	text, _ := responseContentBlock.(*types.ContentBlockMemberText)
+
+	return text.Value, nil
+}
--- backend/main.go
+++ backend/main.go
@@ -4,24 +4,51 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
 	"path/filepath"
 	"strings"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
+type Router struct {
+	ip InferenceProvider
+}
+
+func (rt *Router) hello(w http.ResponseWriter, r *http.Request) {
+	question := "sink broke AGAIN. I'm don paying rent if it isn't fixed for real this time!"
+	resp, err := rt.ip.Infer(r.Context(), question)
+	if err != nil {
+		http.Error(w, "failed to run inference", http.StatusInternalServerError)
+		slog.ErrorContext(r.Context(), "failed to run inference", "err", err)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	type Response struct {
-		Message string `json:"message"`
+		Question string `json:"question"`
+		Response string `json:"response"`
 	}
-	response := Response{Message: "Hello, World!"}
+	response := Response{Question: question, Response: resp}
 	json.NewEncoder(w).Encode(response)
 }
 
 func main() {
-	mux := http.NewServeMux()
+	var ip InferenceProvider
 
-	mux.HandleFunc("GET /api/hello", hello)
+	aws, err := NewAWS()
+	if err != nil {
+		slog.Warn("AWS did not initialize. Falling back to mock provider")
+		ip = NewMockInferenceProvider()
+	} else {
+		ip = aws
+	}
+
+	rt := Router{
+		ip: ip,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /api/hello", rt.hello)
 
 	mux.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ext := filepath.Ext(r.URL.Path)
