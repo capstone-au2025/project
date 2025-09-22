22a4f13fc0956e460e590c2e579bb5cf8337a120
Christopher McDevitt <47703105+RebelAndroid@users.noreply.github.com>
2025-09-22 13:55:35 -0400

    letter generation (#4)

---
+++ backend/letter.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	_ "embed"
+	"encoding/json"
+	"log/slog"
+	"os/exec"
+)
+
+type LetterParams struct {
+	SenderName       string `json:"sender_name"`
+	SenderAddress    string `json:"sender_address"`
+	ReceiverName     string `json:"receiver_name"`
+	ReceiverAddress  string `json:"receiver_address"`
+	ComplaintSummary string `json:"complaint_summary"`
+	LetterContent    string `json:"letter_content"`
+	Date             string `json:"date"`
+}
+
+//go:embed letter-template.typst
+var letterTemplate []byte
+
+func RenderPdf(ctx context.Context, params LetterParams) ([]byte, error) {
+	p, err := json.Marshal(params)
+	if err != nil {
+		return nil, err
+	}
+	cmd := exec.Command("typst", "compile", "-", "-", "--input=params="+string(p))
+
+	in, err := cmd.StdinPipe()
+	if err != nil {
+		return nil, err
+	}
+	_, err = in.Write(letterTemplate)
+	if err != nil {
+		return nil, err
+	}
+	in.Close()
+
+	buf := bytes.Buffer{}
+	cmd.Stdout = &buf
+
+	errBuf := bytes.Buffer{}
+	cmd.Stderr = &errBuf
+
+	err = cmd.Run()
+	if err != nil {
+		slog.ErrorContext(ctx, "failed to run typst", "stderr", errBuf.String(), "err", err)
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
