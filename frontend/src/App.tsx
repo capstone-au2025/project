ad39bb7f607499c1f9d9f5b9de135c72d523890e
Joshua Sims <jm8hjkl@gmail.com>
2025-09-23 10:34:29 -0400

    Remove vite and react logoi

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { sendMail } from "./certifiedmail";
 
@@ -46,34 +43,10 @@ function testSendMail() {
 }
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <button onClick={testSendMail}>Test Send Mail</button>
-
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <button onClick={testSendMail}>Test Send Mail</button>
   );
 }
 
 export default App;
+
